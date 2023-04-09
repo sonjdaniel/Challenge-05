1c592c7359589a4b22210ada92646a880ffe751b
Daniel Son <sonjdaniel@gmail.com>
2023-04-09 13:32:04 -0500

    fix the link in HTML

--- Assets/script.js
+++ Assets/script.js
@@ -19,7 +19,9 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+// TODO: Add code to display the current date in the header of the page.
+
+
 // current day
 var currentDay = dayjs();
 $('#currentDay').text(currentDay.format('dddd, MMMM D YYYY'))
